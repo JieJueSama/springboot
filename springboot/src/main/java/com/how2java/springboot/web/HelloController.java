adce75b81fef12fb3b24e5567a85a051bf1438b1
zhaojiaxin <446212027@qq.com>
2019-11-08 16:12:50 +0800

    springboot thymeleaf

--- springboot/src/main/java/com/how2java/springboot/web/HelloController.java
+++ springboot/src/main/java/com/how2java/springboot/web/HelloController.java
@@ -1,23 +1,17 @@
 package com.how2java.springboot.web;
 
-import java.text.DateFormat;
-import java.util.Date;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
+
 	
 	@RequestMapping("/hello")
-	public String hello(Model m) throws Exception {
-		m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
-		if(true) {
-			throw new Exception("some exception");
-		}
+	public String hello(Model m) {
+		m.addAttribute("name", "thymeleaf");
+		
 		return "hello";
 	}
-
 }
