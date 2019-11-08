974da8834b181ba8f5767305157a3677f0f9a904
zhaojiaxin <446212027@qq.com>
2019-11-08 18:20:47 +0800

    springboot  thymeleaf  url  入门

--- springboot/src/main/java/com/how2java/springboot/Application.java
+++ springboot/src/main/java/com/how2java/springboot/Application.java
@@ -7,8 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 //表示这是一个Springboot应用 ，运行主方法就会启动tomcat，默认端口号8080
 @SpringBootApplication
-//开启缓存
-@EnableCaching
 public class Application {
 	
 	public static void main(String[] args) {
